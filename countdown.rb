c35348f5f1525faed56782fb061e4ecdc45895fb
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 16:32:13 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
+def countdown(x)
+  x = 10
+  while x>0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(x)
+  x = 10
+  while x>0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  end
+    return "HAPPY NEW YEAR!"
 end
